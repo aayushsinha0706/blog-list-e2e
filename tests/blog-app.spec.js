6943d9ea7d7924277d687d790e27b11a30f8335a
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2025-03-21 23:21:19 +0530

    5.18: Blog List End To End Testing, step 2

--- tests/blog-app.spec.js
+++ tests/blog-app.spec.js
@@ -1,7 +1,15 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-
+const { loginWith } = require('./helper')
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users',{
+        data: {
+            name: 'Aayush Sinha',
+            username: 'aayushsinha0706',
+            password: 'aayushsinha0706'
+        }
+    })
     await page.goto('/')
   })
 
@@ -11,5 +19,23 @@ describe('Blog app', () => {
     const password = await page.getByTestId('password')
     expect(username).toBeVisible()
     expect(password).toBeVisible()
+  })
+
+  describe('Login', () => {
+
+    test('succeeds with correct credentials', async ({page}) => {
+        await loginWith(page, 'aayushsinha0706', 'aayushsinha0706')
+        await expect(page.getByText('Aayush Sinha is logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({page}) => {
+        await loginWith(page, 'aayushsinha0706', 'wrongpassword')
+
+        const errorDiv = await page.getByTestId('message')
+        await expect(errorDiv).toContainText('Wrong username or password')
+        await expect(errorDiv).toHaveCSS('border-style', 'solid')
+        await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
+
   })
 })
